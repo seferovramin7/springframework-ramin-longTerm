c7c1739191dc79414fc6f25b86c6929ae9573a6b
seferovramin7 <seferovramin7@gmail.com>
2020-04-19 15:05:18 +0400

    Initial commit

---
+++ src/main/java/ramin/springpet/sfgpet/SfgPetApplication.java
@@ -0,0 +1,13 @@
+package ramin.springpet.sfgpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetApplication.class, args);
+    }
+
+}
