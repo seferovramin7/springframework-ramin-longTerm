9c0f5559330fe8e2a1c3e2fa4e8f31a1e717ec3f
seferovramin7 <seferovramin7@gmail.com>
2020-04-28 10:12:38 +0400

    Refactoring Id generation Map implamantation Closes #23

--- pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
@@ -26,14 +26,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Murad");
-        owner1.setLastName("Heyderov");
+        owner1.setLastName("Heyde rov");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Eli");
         owner2.setLastName("babayev");
 
@@ -42,14 +40,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Same");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Same");
         vet2.setLastName("Houston");
 
