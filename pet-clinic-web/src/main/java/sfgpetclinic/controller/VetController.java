f4c43823c2daedd9119b7aaf3c88ab30a9e55737
seferovramin7 <seferovramin7@gmail.com>
2020-04-28 09:39:53 +0400

    Added vet list and VetController Closes #15

--- pet-clinic-web/src/main/java/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,22 @@
 package sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgpetclinic.service.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 
