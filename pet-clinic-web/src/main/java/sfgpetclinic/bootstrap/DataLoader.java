3995e32e0e25346343f2de778a41d7d866fe714a
seferovramin7 <seferovramin7@gmail.com>
2020-05-01 12:53:57 +0400

    Added Pets Contact info on startup   Closes #33

--- pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,11 +4,14 @@ package sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import sfgpetclinic.model.Owner;
+import sfgpetclinic.model.Pet;
 import sfgpetclinic.model.PetType;
 import sfgpetclinic.model.Vet;
 import sfgpetclinic.service.OwnerService;
 import sfgpetclinic.service.PetTypeService;
 import sfgpetclinic.service.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -39,12 +42,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Murad");
         owner1.setLastName("Heyderov");
+        owner1.setAddress("Heyder Eliyev st.");
+        owner1.setCity("Baku");
+        owner1.setTelephone("051-898-07-77");
+
+        Pet muradsDog = new Pet();
+        muradsDog.setPetType(savedDogType);
+        muradsDog.setOwner(owner1);
+        muradsDog.setBirthDate(LocalDate.now());
+        muradsDog.setName("Rosco");
+        owner1.getPets().add(muradsDog);
+
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Eli");
         owner2.setLastName("Babayev");
+        owner2.setAddress("Bextiyar Vahabzade st.");
+        owner2.setCity("NAxcivan");
+        owner2.setTelephone("055-345-43-03");
+
+        Pet eliSCat = new Pet();
+        eliSCat.setPetType(savedCatType);
+        eliSCat.setOwner(owner2);
+        eliSCat.setBirthDate(LocalDate.now());
+        eliSCat.setName("Meow");
+        owner2.getPets().add(eliSCat);
 
         ownerService.save(owner2);
 
